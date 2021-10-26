c095af5a91ee17c4d24b1c94b4b5f58e01000fbd
6icada <84038008+6icada@users.noreply.github.com>
2021-10-26 21:29:30 +0400

    Update BTC-TO-GEL.py

--- BTC-TO-GEL.py
+++ BTC-TO-GEL.py
@@ -5,27 +5,51 @@
 import requests
 import os
 from bs4 import BeautifulSoup
+from requests.api import request
 
 # GET request to site to get html script
-response = requests.get('https://valuta.exchange/ka/btc-to-gel?amount=1')
+responseGEL = requests.get('https://valuta.exchange/ka/btc-to-gel?amount=1')
+responseUSD = requests.get('https://valuta.exchange/ka/btc-to-usd?amount=1')
 
-# Opening(Making) file to paste html script
-f = open('response.html', 'w')
-f.write(f'{response.text}')
-f.close()
+print('1) GEL')
+print('2) USD')
+# userInput to choose what currency he/she wants
+userInput = input('Enter what currency you want: ')
 
-# Reading html file with 
-with open('response.html', 'r') as f:
-    document = BeautifulSoup(f, 'html.parser')
+if userInput == 1 or userInput == '1':
+    # Opening(Making) file to paste html script
+    f = open('responseGEL.html', 'w')
+    f.write(f'{responseGEL.text}')
+    f.close()
 
-# Making vars
-tag = document.find_all(text='GEL')
-parent = tag[0].parent
-span = parent.find('span')
+    # Reading html file with BeautifulSoup
+    with open('responseGEL.html', 'r') as f:
+        document = BeautifulSoup(f, 'html.parser')
 
-# Printing result
-print(f'Lari(GEL) is Georgian currency')
-print(f'BTC to GEL price is: {span.string} Lari')
+    # Making vars
+    tag = document.find_all(text='GEL')
+    parent = tag[0].parent
+    span = parent.find('span')
 
-# Removing html file
-os.system('rm -f response.html 2&1> /dev/null')
+    # Printing result
+    print(f'BTC to GEL price is: {span.string} Lari')
+
+    # Removing html file
+    os.system('rm -f responseGEL.html 2&1> /dev/null')
+elif userInput == 2 or userInput == '2':
+    # Opening(Making) file to paste html script
+    f = open('responseUSD.html', 'w')
+    f.write(f'{responseUSD.text}')
+    f.close()
+
+    # Reading html file with BeautifulSoup
+    with open('responseUSD.html', 'r') as f:
+        document = BeautifulSoup(f, 'html.parser')
+    
+    # Making vars
+    tag = document.find_all(text='USD')
+    parent = tag[0].parent
+    span = parent.find('span')
+
+    # Printing result
+    print(f'BTC to USD price is: {span.string} Lari')
